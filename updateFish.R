c9f7766a24369cc4564695d1d5817a60b85754a4
brittnibertolet <bbertole@nd.edu>
2019-06-13 18:46:35 -0500

    added directories for lab computer to the updateFish.R file

--- updateFish.R
+++ updateFish.R
@@ -5,13 +5,20 @@
 # run the code below to update fish data with new datasheets that have been entered into the template
 # and saved as .csv files
 
-# Load the entry function
-source('fishEntry.R')
+#setwd("C:/Users/jones/OneDrive/temp_fishscapesCSVentries") #UNDERC lab computer wd
 
-# set variables
-dbdir="C:/Users/jones/Box Sync/NDstuff/ND_R/"
+# Load the entry function
+source('C:/Users/jones/Documents/Summer2019/Fish/fishEntryTool/fishEntry.R')
+
+# # set variables - Colin's computer
+# dbdir="C:/Users/jones/Box Sync/NDstuff/ND_R/"
+# db="MFEdb.db"
+# funcdir="C:/Users/jones/Box Sync/NDstuff/ND_R/fishEntryTool/"
+
+# set variables - UNDERC lab computer
+dbdir="C:/Users/jones/Documents/Summer2019/Database/current/"
 db="MFEdb.db"
-funcdir="C:/Users/jones/Box Sync/NDstuff/ND_R/fishEntryTool/"
+funcdir="C:/Users/jones/Documents/Summer2019/Database/dbFunctions/"
 
 #update
-updateFish(dbdir=dbdir,db=db,funcdir=funcdir, force_siteID = T, force_sampleGroup = T, force_effortUnits = T, force_metadataID = T, force_clip = T)
+updateFish(dbdir=dbdir,db=db,funcdir=funcdir)
