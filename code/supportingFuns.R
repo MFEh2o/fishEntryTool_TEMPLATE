2a8772cd33b08eeca3cec1eef0fc8cd649974f59
Kaija Gahm (cary) <kaijagahm@Kaijas-MBP.lan>
2021-05-21 11:41:58 -0400

    updates to the getHeader fun

--- code/supportingFuns.R
+++ code/supportingFuns.R
@@ -11,11 +11,20 @@ getHeader <- function(d = cur){
     pivot_wider(names_from = key, values_from = value) %>%
     as.list()
   
+  # Fix the "dataTimeSet" typo if it occurs
+  names(header) <- str_replace(names(header), "dataTimeSet", "dateTimeSet")
+  
+  # Apply some formatting and coercion
   header$projectID <- as.numeric(header$projectID)
   header$dateTimeSet <- paste0(header$dateTimeSet, ":00")
   header$dateTimeSample <- paste0(header$dateTimeSample, ":00")
-  
-  
+  header$crew <- str_replace_all(header$crew, ",", ", ") %>%
+    str_replace_all(., "\\s\\s", " ")
+  # XXX should enforce a constraint on gear here
+  header$distanceShocked <- ifelse(header$gear == "BE", header$distanceShocked, NA)
+  header$effort <- as.numeric(header$effort)
+  # XXX should also enforce constraints on effort units
+
   # Return the header list
   return(header)
 }
