5d1beb9b3a9ad2c91b0f76ce2da21dea8f05acf0
Kaija Gahm (cary) <kaijagahm@kaijas-mbp.lan>
2021-06-21 16:31:55 -0400

    reformatted function call, and made some annotations

--- code/updateFish.R
+++ code/updateFish.R
@@ -6,13 +6,16 @@
 library(tidyverse)
 library(here)
 
+# Show long errors and warnings
+options(warning.length = 10000L, error.length = 10000L)
+
 # Set file paths ----------------------------------------------------------
 # File paths are relative paths, written with the here` package
 dbdir <- here()
 db <- "MFEdb_20210528.db" # name of the database file you're using. Try to use one with a specific date to make the workflow clear. For example, "MFEdb_20200530.db"
 funcdir <- here("code") # directory where the code/functions is stored
 isdir <- here("inSeason") # directory where the in-season db files will be stored
-ssdir <- here("sampleSheets_newFormats")
+ssdir <- here("sampleSheets")
 
 # Run the code below to update fish data with new datasheets that have been entered into the template and saved as .csv files
 
@@ -20,5 +23,14 @@ ssdir <- here("sampleSheets_newFormats")
 source(here("code", "fishEntry.R"))
 
 # Run code to update in-season files
-updateFish(headerRows = 18, dbdir = dbdir, db = db, funcdir = funcdir, 
-           isdir = isdir, ssdir = ssdir) 
+## Note about headerRows: 18 is the default number of headerRows present in the normal fish data sheets (i.e. non-minnow trap sheets). The tool takes the headerRows number and subtracts 1 to account for the slightly shorter minnow trap header.
+updateFish(headerRows = 18, 
+           # the following arguments are defined above:
+           dbdir = dbdir, 
+           db = db, 
+           funcdir = funcdir, 
+           isdir = isdir, 
+           ssdir = ssdir
+           # add any force_* arguments here
+           ) 
+
